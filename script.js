2e9c45148a6ee407978e6fc3fa9f343716a1a6be
Miguel Moreno <miguel.morenor@icloud.com>
2023-06-27 15:15:17 -0500

    responsive web design

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ let changeText = () =>{
 changeText();
 setInterval(changeText,3000)
 
-// circle skill ///
+// circle skill---------------------------------------------------------------------------- ///
 const circles = document.querySelectorAll('.circle');
 circles.forEach(elem=>{
     var dots = elem.getAttribute("data-dots");
@@ -53,3 +53,43 @@ circles.forEach(elem=>{
         pointsMarked[i]. classList.add('marked')
     }
 })
+
+// mix it up portfolio section
+var mixer = mixitup('.portfolio-gallery');
+
+
+// active menu---------------------------------------------------------------------------- ///
+
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section')
+
+function activeMenu(){
+    let len= section.length;
+    while(--len && window.scrollY + 97 < section [len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove('active'));
+    menuLi[len].classList.add('active');
+}
+
+activeMenu();
+window.addEventListener('scroll', activeMenu);
+
+// sticky navbar---------------------------------------------------------------------------- ///
+
+const header = document.querySelector('header');
+window.addEventListener('scroll', function(){
+    header.classList.toggle('sticky', window.scrollY > 50);
+})
+
+// toggle icon navbar---------------------------------------------------------------------------- ///
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menuIcon.onclick = ()=> {
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
+
+window.onscroll = ()=> {
+    menuIcon.classList.remove("bx-x");
+    navlist.classList.remove("open");
+}
